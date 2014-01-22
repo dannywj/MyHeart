4e669a095cdf4b888b913119ad220156786809b8
wangjue_os <wangjue_os@lightinthebox.com>
2014-01-22 16:15:58 +0800

    添加GeGe正式数据 FromBeginTo0121

--- MyHeart/DBScripts/ht.createTable.sql
+++ MyHeart/DBScripts/ht.createTable.sql
@@ -24,12 +24,14 @@ GO
 CREATE TABLE [dbo].[ht_message]
 (
 [id] [int] NOT NULL IDENTITY(1, 1),
+[pub_date] [datetime] NULL,
 [writer] [nvarchar] (50) COLLATE Chinese_PRC_CI_AS NULL,
-[content] [nvarchar] (4000) COLLATE Chinese_PRC_CI_AS NULL,
-[pub_date] [datetime] NULL
+[content] [nvarchar] (4000) COLLATE Chinese_PRC_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[ht_message] ADD CONSTRAINT [PK_ht_message] PRIMARY KEY CLUSTERED  ([id]) ON [PRIMARY]
 GO
 
-select * from [ht_message]
+SELECT Convert(varchar(10),[pub_date],120),[writer],[content]
+FROM [heart].[dbo].[ht_message]
+ORDER BY id desc
