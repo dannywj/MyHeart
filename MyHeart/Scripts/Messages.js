55a383032c7bddb46f596e607963286b6a6bd4e5
dannywang <danny__wj@163.com>
2014-07-23 12:02:39 +0800

    修改message日期控件默认值，autofocus

--- MyHeart/Scripts/Messages.js
+++ MyHeart/Scripts/Messages.js
@@ -8,6 +8,22 @@ String.prototype.replaceAll = function (s1, s2) {
     return this.replace(new RegExp(s1, "gm"), s2);
 }
 
+Date.prototype.Format = function (fmt) { //author: meizz
+    var o = {
+        "M+": this.getMonth() + 1, //月份
+        "d+": this.getDate(), //日
+        "h+": this.getHours(), //小时
+        "m+": this.getMinutes(), //分
+        "s+": this.getSeconds(), //秒
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+        "S": this.getMilliseconds() //毫秒
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+};
+
 $(function () {
     //搜索事件
     $("#btnSearch").click(function () {
@@ -32,7 +48,7 @@ $(function () {
     });
 
     //初始化日期控件
-    $("#txtDate").datepicker({ minDate: '2013-10-07' });
+    $("#txtDate").datepicker({ minDate: '2013-10-07',maxDate:new Date().Format("yyyy-MM-dd") });
 
     //获取随机消息
     function getData() {
@@ -94,6 +110,7 @@ $(function () {
                 $('.hide_content').fadeOut(400);
             }
         });
+        $("#txtPwd").focus();
     }
 
     initPage();
